7b18f959403bb6be6f1113c8fbdd03dec79a12af
Richa Mittal <richa.mittal@metacube.com>
2024-09-06 01:49:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,35 @@ try {
   const end = core.getInput("end");
 
   let result = "";
+  let smallest_error = "";
   if (string) {
     const pos = string.indexOf(start) + start.length;
     result = string.substring(pos, string.indexOf(end, pos));
+	var errorListStr = result.split('All Component Failures:\n')[1];
+	smallest_error = getErrorWithMinChars(errorListStr.split('\n'));
   }
 
   core.setOutput("substring", result);
+  core.setOutput("smallest_error", smallest_error);
 } catch (error) {
   core.setFailed(error.message);
 }
+
+function getErrorWithMinChars(arr) {
+    if (arr.length === 0) {
+        return "";
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return arr[maxIndex];
+}
+
